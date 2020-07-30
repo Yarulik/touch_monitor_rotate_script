903aad3755c09d935939ea665bd76e554ad98434
Yaroslav Litvinov <yarulik1986@gmail.com>
2020-07-30 11:03:29 +0500

    Add files via upload

---
+++ monitor_rotate_script/monitor_normal.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#landscape (normal)
+xrandr -o left
+xinput set-prop "Nuvoton w836x7hg Infrared Remote Transceiver" --type=float "Coordinate Transformation Matrix" 0 0 0 0 0 0 0 0 0
+xinput set-prop "eGalaxTouch Virtual Device for Single" --type=float "Coordinate Transformation Matrix" 0 0 0 0 0 0 0 0 0
+xinput set-prop "eGalax Inc. eGalaxTouch EXC3188-2208-05.00.00" --type=float "Coordinate Transformation Matrix" 0 0 0 0 0 0 0 0 0
+xinput set-prop "eGalaxTouch Virtual Device for Multi" --type=float "Coordinate Transformation Matrix" 0 0 0 0 0 0 0 0 0
